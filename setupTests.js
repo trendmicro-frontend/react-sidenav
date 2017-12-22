cf2a3869040ed521fe7f703b55f97e2be29371de
Cheton Wu <cheton@gmail.com>
2017-12-22 18:40:26 +0800

    Setup tests

---
+++ setupTests.js
@@ -0,0 +1,33 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM } from 'jsdom';
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+// Ignore `.styl` files
+require.extensions['.styl'] = () => {
+    return;
+};
+
+// JSDOM
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+const copyProps = (src, target) => {
+    const props = Object.getOwnPropertyNames(src)
+        .filter(prop => typeof target[prop] === 'undefined')
+        .reduce((result, prop) => ({
+            ...result,
+            [prop]: Object.getOwnPropertyDescriptor(src, prop),
+        }), {});
+    Object.defineProperties(target, props);
+};
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+    userAgent: 'node.js',
+};
+
+copyProps(window, global);
--- test/index.js
+++ test/index.js
@@ -1,5 +1,25 @@
+import React from 'react';
+import { mount } from 'enzyme';
 import { test } from 'tap';
+import '../setupTests';
+import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '../src';
 
-test('noop', (t) => {
+test('<SideNav />', (t) => {
+    const wrapper = mount((
+        <SideNav>
+            <Toggle />
+            <Nav>
+                <NavItem>
+                    <NavIcon />
+                    <NavText />
+                </NavItem>
+                <NavItem>
+                    <NavIcon />
+                    <NavText />
+                </NavItem>
+            </Nav>
+        </SideNav>
+    ));
+    t.equal(wrapper.find(SideNav).length, 1, 'should render <SideNav /> component');
     t.end();
 });
